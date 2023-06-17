c7ff756125df4719970eeb268ac2b58dc285613e
nopecode96 <nopecode96@gmail.com>
2023-06-17 19:00:30 +0700

    update commission

--- app/controllers/marketing/commission.controller.js
+++ app/controllers/marketing/commission.controller.js
@@ -6,74 +6,20 @@ const async = require('async')
 var functions = require("../../../config/function");
 const { commission, events, eventsGallery, eventsGiftBank, eventsGuest, eventsTicketing, eventsWedding, company, regRegencies, regProvincies, masterEvent } = require("../../models/index.model");
 
-exports.getTotalBoard = (req, res) => {
-    const { user_id } = req.query;
-
-    commission.findAll({
-        where: { fid_user: user_id },
-        limit: 1,
-        order: [['id', 'DESC']],
-    })
-        .then(data => {
-            const balances = data.balance;
-            commission.findAll({
-                where: { fid_user: user_id, action: 'IN' },
-                attributes: [
-                    [sequelize.fn("SUM", sequelize.cast(sequelize.col("nominal"), 'integer')), "total_in"],
-                ],
-            })
-                .then(data1 => {
-                    const balance = balances;
-                    const totalIn = data1[0].dataValues.total_in;
-                    commission.findAll({
-                        where: { fid_user: user_id, action: 'OUT' },
-                        attributes: [
-                            [sequelize.fn("SUM", sequelize.cast(sequelize.col("nominal"), 'integer')), "total_out"],
-                        ],
-                    })
-                        .then(data2 => {
-                            const totalOut = data2[0].dataValues.total_out;
-                            // console.log(data2[0].balance)
-                            // console.log(data2[0].balance);
-
-                            res.status(200).send({
-                                code: 200,
-                                success: true,
-                                message: "Datas Found.",
-                                data: {
-                                    total_in: totalIn == null ? 0 : parseInt(totalIn),
-                                    total_out: totalOut == null ? 0 : parseInt(totalOut),
-                                    balance: balance == null ? 0 : balance
-                                }
-                            });
-                        })
-
-                })
-                .catch(err => {
-                    // console.log(err);
-                    res.status(500).send({
-                        code: 500,
-                        success: false,
-                        message:
-                            err.message || "Some error occurred while retrieving data."
-                    });
-                });
-        })
-}
-
 exports.getCommissionList = (req, res) => {
     const fid_user = req.userid;
     const { page, size } = req.query;
     const { limit, offset } = functions.getPagination(page - 1, size);
 
     var condition = {
-        fid_user: fid_user
+        fid_user: fid_user,
+        status: 'SATTLE'
     }
 
     async.parallel({
         totalIn: function (callback) {
             commission.findAll({
-                where: { fid_user: fid_user, action: 'IN' },
+                where: { fid_user: fid_user, action: 'IN', status: 'SATTLE' },
                 attributes: [
                     [sequelize.fn("SUM", sequelize.cast(sequelize.col("nominal"), 'integer')), "total_in"],
                 ],
@@ -81,7 +27,7 @@ exports.getCommissionList = (req, res) => {
         },
         totalOut: function (callback) {
             commission.findAll({
-                where: { fid_user: fid_user, action: 'OUT' },
+                where: { fid_user: fid_user, action: 'OUT', status: 'SATTLE' },
                 attributes: [
                     [sequelize.fn("SUM", sequelize.cast(sequelize.col("nominal"), 'integer')), "total_out"],
                 ],
@@ -89,7 +35,7 @@ exports.getCommissionList = (req, res) => {
         },
         balance: function (callback) {
             commission.findAll({
-                where: { fid_user: fid_user },
+                where: { fid_user: fid_user, status: 'SATTLE' },
                 limit: 1,
                 order: [['id', 'DESC']],
             }).then(data => callback(null, data))
@@ -114,7 +60,7 @@ exports.getCommissionList = (req, res) => {
         }
         const totalIn = results.totalIn[0].dataValues.total_in ?? 0
         const totalOut = results.totalOut[0].dataValues.total_out ?? 0
-        const balance = results.totalOut[0].dataValues.total_out ?? 0
+        const balance = results.balance[0].dataValues.balance ?? 0
 
         console.log(totalIn);
 
@@ -132,5 +78,21 @@ exports.getCommissionList = (req, res) => {
             }
         })
         return;
+    })
+}
+
+exports.getWithdrawPage = (req, res) => {
+    const fid_user = req.userid;
+
+    async.parallel({
+        dataEvents: function (callback) {
+            events.findAll({
+                where: { fid_user: fid_user },
+                attributes: [
+                    [sequelize.fn("SUM", sequelize.cast(sequelize.col("nominal"), 'integer')), "total_in"],
+                ],
+            }).then(data => callback(null, data))
+        },
+
     })
 }
