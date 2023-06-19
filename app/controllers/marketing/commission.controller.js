b6b89e9637be580e573347913a5e5f7e0edc1de8
nopecode96 <nopecode96@gmail.com>
2023-06-19 18:02:13 +0700

    lll

--- app/controllers/marketing/commission.controller.js
+++ app/controllers/marketing/commission.controller.js
@@ -1,10 +1,11 @@
 const db = require("../../models/index.model");
 const sequelize = require('sequelize');
 const fs = require("fs");
-const async = require('async')
+const async = require('async');
+var randomstring = require("randomstring");
 
 var functions = require("../../../config/function");
-const { commission, masterBank, userProfile } = require("../../models/index.model");
+const { commission, masterBank, userProfile, commissionWithdraw } = require("../../models/index.model");
 
 exports.getCommissionList = (req, res) => {
     const fid_user = req.userid;
@@ -81,7 +82,40 @@ exports.getCommissionList = (req, res) => {
     })
 }
 
-exports.getWidrawalPage = (req, res) => {
+exports.getWidrawalList = (req, res) => {
+    const fid_user = req.userid;
+    const { page, size } = req.query;
+    const { limit, offset } = functions.getPagination(page - 1, size);
+
+    var condition = {
+        fid_user: fid_user
+    }
+
+    commissionWithdraw.findAndCountAll({
+        where: condition, limit, offset,
+    }).then(data => {
+        // console.log(data);
+        const response = functions.getPagingData(data, page, limit);
+        // console.log(response);
+        res.status(200).send({
+            code: 200,
+            success: true,
+            message: 'Data Found',
+            data: response
+        })
+        return;
+    }).catch(err => {
+        res.status(500).send({
+            code: 500,
+            success: false,
+            message:
+                err.message || "Some error occurred while retrieving data."
+        });
+        return;
+    });
+}
+
+exports.getWidrawalCreatePage = (req, res) => {
     const fid_user = req.userid;
 
     async.parallel({
@@ -119,13 +153,73 @@ exports.getWidrawalPage = (req, res) => {
             success: true,
             message: 'Data Found',
             data: {
-
                 balance: balance,
                 userBankAccount: results.userBankAccount[0],
-
             }
         })
         return;
     })
 }
 
+exports.WidrawalCreate = (req, res) => {
+    const fid_user = req.userid;
+    const status = 'PENDING';
+    const wd_number = randomstring.generate({
+        length: 6,
+        capitalization: 'uppercase'
+    });
+
+    const { nominal, bank_name, bank_account_name, bank_account_number } = req.body;
+
+    commission.findAll({
+        where: { fid_user: fid_user, status: 'SATTLE' },
+        limit: 1,
+        order: [['id', 'DESC']],
+    }).then(data => {
+        const balancex = parseFloat(data[0].balance);
+        const nominalx = parseFloat(nominal);
+
+        if (nominalx >= balancex) {
+            res.status(404).send({
+                code: 404,
+                success: false,
+                message: 'Your balance not enough, please check your nominal!',
+            })
+            return;
+        }
+
+        commissionWithdraw.create({ wd_number, nominal, bank_name, bank_account_name, bank_account_number, status, fid_user })
+            .then(data => {
+                if (data.length == 0) {
+                    res.status(404).send({
+                        code: 404,
+                        success: false,
+                        message: "Insert data false.",
+                        // insertID: data.order_number
+                    });
+                    return;
+                }
+
+                res.status(200).send({
+                    code: 200,
+                    success: true,
+                    message: "Widthdrawal has been created.",
+                    wd_number: data.wd_number
+                });
+                return;
+
+            }).catch(err => {
+                // console.log(err);
+                res.status(500).send({
+                    code: 500,
+                    success: false,
+                    message:
+                        err || "Some error occurred while retrieving data."
+                });
+                return;
+            });
+
+    })
+
+
+}
--- app/routes/marketing/commission.routes.js
+++ app/routes/marketing/commission.routes.js
@@ -5,7 +5,9 @@ module.exports = app => {
 
     // router.get("/totalboard", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.getTotalBoard);
     router.get("/", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.getCommissionList);
-    router.get("/widrawal-page", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.getWidrawalPage);
+    router.get("/widrawal-create-page", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.getWidrawalCreatePage);
+    router.get("/widrawal-list", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.getWidrawalList);
+    router.post("/widrawal-create", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.WidrawalCreate);
 
     app.use('/v2/marketing/commission', router);
 }
