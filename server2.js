13353f16c961d5149383ae29dbb46bfcdd22120d
nopecode96 <nopecode96@gmail.com>
2023-07-20 21:59:03 +0700

    update

--- server2.js
+++ server2.js
@@ -80,6 +80,8 @@ require("./app/routes/website/wedding.routes")(app);
 require("./app/routes/website/marketing.routes")(app);
 require("./app/routes/thirdparty/motasi.routes")(app);
 
+require("./app/routes/scanner/scanner.routes")(app);
+
 dotenv.config();
 
 app.get("/" + process.env.ENVIRONMENT + "/", (req, res) => {
