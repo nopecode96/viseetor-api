e580e36da1356fda58d670435a4a7e65c237e0a8
nopecode96 <nopecode96@gmail.com>
2023-07-24 12:15:32 +0700

    sss

--- app/controllers/scanner/access.controller.js
+++ app/controllers/scanner/access.controller.js
@@ -92,7 +92,7 @@ exports.getHomeData = (req, res) => {
         dataEvent: function (callback) {
             events.findAll({
                 where: { id: fid_events },
-                attributes: ['id', 'title', 'description', 'banner', 'venue_name', 'event_date', 'location_address'],
+                attributes: ['id', 'barcode', 'title', 'description', 'banner', 'venue_name', 'event_date', 'location_address'],
                 include: [
                     {
                         model: regRegencies,
