5eb9e6c8159798b501f82a87685dcc107abba322
nopecode96 <nopecode96@gmail.com>
2023-07-25 10:40:36 +0700

    ss

--- app/routes/scanner/scanner.routes.js
+++ app/routes/scanner/scanner.routes.js
@@ -9,7 +9,7 @@ module.exports = app => {
     router.post("/access", authValidation.apiKeyValidation, controller.login);
     router.get("/home-data", authValidation.apiKeyValidation, authValidation.tokenMobileScannerAppValidation, controller.getHomeData);
     router.get("/guestlist", authValidation.apiKeyValidation, authValidation.tokenMobileScannerAppValidation, controller.getGuestList);
-    router.put("/scan", authValidation.apiKeyValidation, authValidation.tokenMobileScannerAppValidation, controller.scanBarcode);
+    router.get("/scan", authValidation.apiKeyValidation, authValidation.tokenMobileScannerAppValidation, controller.scanBarcode);
     router.put("/update-status", authValidation.apiKeyValidation, authValidation.tokenMobileScannerAppValidation, controller.updateStatus);
     app.use('/' + process.env.ENVIRONMENT + '/scanner', router);
 };
