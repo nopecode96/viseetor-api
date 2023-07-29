99cab1b4dd7447a64ae806710077c16edd7c6bfa
Gus Tho <agus.thaiba@btpn.com>
2023-07-29 22:08:37 +0700

    chore(env-logs): add example.env and logger system

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    DB: process.env.DB_NAME,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    HOST: process.env.DB_HOST,
+    PORT: 5432,
+    dialect: 'postgres',
+    pool: {
+        min: 0,
+        max: 10,
+        acquire: 60.000,
+        idle: 10000
+    }
+}
