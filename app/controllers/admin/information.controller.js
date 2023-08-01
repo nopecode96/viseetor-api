1bdfee359aa2188f371f1de0413fc58beefdb486
nopecode96 <nopecode96@gmail.com>
2023-08-01 12:04:22 +0700

    update eveent

--- app/controllers/admin/information.controller.js
+++ app/controllers/admin/information.controller.js
@@ -4,10 +4,31 @@ const md5 = require('md5');
 const async = require('async')
 const axios = require("axios")
 
+const { promotion, users } = require("../../models/index.model");
+
 exports.getInformation = (req, res) => {
 
 }
 
 exports.getPrommo = (req, res) => {
-    
+    const fid_user = req.userid;
+    const { page, size } = req.query;
+    const { limit, offset } = functions.getPagination(page - 1, size);
+
+    promotion.findAll({
+        where: limit, offset,
+        order: [['updatedAt', 'DESC']],
+        include: {
+            model: users,
+            attributes: ['id', 'name']
+        }
+    }).then(data => {
+        res.status(200).send({
+            code: 200,
+            success: true,
+            message: 'Data found',
+            data: data
+        })
+        return;
+    })
 }
