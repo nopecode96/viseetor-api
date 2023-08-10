d691327c56a9f6d77c43f3f4af34c9c93ce18c26
Gus Tho <agus.thaiba@btpn.com>
2023-08-11 01:55:42 +0700

    feature(db): migration alter table events wedding

--- migrations/20230810175554-alter_table_events_weddings.js
+++ migrations/20230810175554-alter_table_events_weddings.js
@@ -2,22 +2,27 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('transactions', { unix: Sequelize.DOUBLE });
+  async up (queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+
+    return queryInterface.addColumn('events_weddings', 'marriage_time', {
+      type: Sequelize.DataTypes.TIME
+    });
   },
 
-  async down(queryInterface, Sequelize) {
+  async down (queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
+
+    return queryInterface.removeColumn('events_weddings', 'marriage_time');
   }
 };
