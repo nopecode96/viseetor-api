9268661b1b42c7e67301cd0199891ad830cdf06e
nopecode96 <nopecode96@gmail.com>
2023-08-16 12:13:47 +0700

    update auth

--- app/routes/admin/user.routes.js
+++ app/routes/admin/user.routes.js
@@ -6,13 +6,13 @@ module.exports = app => {
     const controller = require("../../controllers/admin/users.controller");
     var router = require("express").Router();
 
-    router.get("/", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.findAllUsersAdmin);
-    router.get("/marketing", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.findAllMarketing);
-    router.post("/marketing", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.createUserMarketing);
-    router.get("/spv", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.findAllSpv);
-    router.post("/spv", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.createUserSpv);
-    router.put("/update-status", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.updateStatusUser);
-    router.get("/detail", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.getAccountDetail);
-    router.put("/change-password", authValidation.apiKeyValidation, authValidation.tokenValidation, controller.changePassword);
+    router.get("/", authValidation.apiKeyValidation, authValidation.tokenAdminValidation, controller.findAllUsersAdmin);
+    router.get("/marketing", authValidation.apiKeyValidation, authValidation.tokenAdminValidation, controller.findAllMarketing);
+    router.post("/marketing", authValidation.apiKeyValidation, authValidation.tokenAdminValidation, controller.createUserMarketing);
+    router.get("/spv", authValidation.apiKeyValidation, authValidation.tokenAdminValidation, controller.findAllSpv);
+    router.post("/spv", authValidation.apiKeyValidation, authValidation.tokenAdminValidation, controller.createUserSpv);
+    router.put("/update-status", authValidation.apiKeyValidation, authValidation.tokenAdminValidation, controller.updateStatusUser);
+    router.get("/detail", authValidation.apiKeyValidation, authValidation.tokenAdminValidation, controller.getAccountDetail);
+    router.put("/change-password", authValidation.apiKeyValidation, authValidation.tokenAdminValidation, controller.changePassword);
     app.use('/' + process.env.ENVIRONMENT + '/admin/users', router);
 };
