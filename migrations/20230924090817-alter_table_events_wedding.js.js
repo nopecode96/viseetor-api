35fa595b9cad3d2379794b16ae49dd6ceb9a0a6b
Nope Lavanda <nopecode96@gmail.com>
2023-09-24 21:38:05 +0700

    update

---
+++ migrations/20230924090817-alter_table_events_wedding.js.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.removeColumn('events_weddings', 'marriage_time'),
+      queryInterface.addColumn('events_weddings', 'marriage_location_address', {
+        type: Sequelize.DataTypes.TEXT,
+      }),
+      queryInterface.addColumn('events_weddings', 'marriage_location_map', {
+        type: Sequelize.DataTypes.TEXT,
+      }),
+      queryInterface.addColumn('events_weddings', 'ceremony_type', {
+        type: Sequelize.DataTypes.ENUM,
+        values: ['None', 'Akad Nikah', 'Pemberkatan', 'Ceremony']
+      }),
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all([
+      // queryInterface.removeColumn('events_weddings', 'marriage_time'),
+      queryInterface.removeColumn('events_weddings', 'marriage_location_address'),
+      queryInterface.removeColumn('events_weddings', 'marriage_location_map'),
+      queryInterface.removeColumn('events_weddings', 'ceremony_type')
+    ]);
+
+  }
+};
---
+++ migrations/20230924110941-alter_table_events.js.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn('events', 'location_map', {
+        type: Sequelize.DataTypes.STRING,
+      }),
+    ]);
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all([
+      queryInterface.removeColumn('events', 'location_map')
+    ]);
+  }
+};
